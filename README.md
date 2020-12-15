f8bc6dad489fa5f7441e76613611e04b50487689
thanapun <31621454+thanapun@users.noreply.github.com>
2020-12-15 17:03:28 +0700

    v1.7

---
+++ README.md
@@ -0,0 +1,8 @@
+Calculate distance
+็How to
+
+import Calculatedistance from 'tvofiller';
+
+Calculatedistance(latitude1, longitude1, latitude2, longitude2, unit)
+
+** choose unit = {K,N}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-module.exports = (arr, condition) => {
-    arr.filter((item) => {
-        return item.search.includes(condition+'')
-    })
+module.exports = (lat1, lon1, lat2, lon2, unit) => {
+    let theta = lon1 - lon2;
+    let dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+    dist = Math.acos(dist);
+    dist = rad2deg(dist);
+    dist = dist * 60 * 1.1515;
+    if (unit == "K") {
+        dist = dist * 1.609344;
+    } else if (unit == "N") {
+        dist = dist * 0.8684;
+    }
+    return (dist);
+}
+function deg2rad(deg) {
+    return (deg * Math.PI / 180.0);
+}
+function rad2deg(rad) {
+    return (rad * 180 / Math.PI);
 }
